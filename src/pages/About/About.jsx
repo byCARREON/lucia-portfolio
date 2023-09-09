eed66a8f33ade25ba2994b3329df468102ed77a2
byCARREON <j8carreon@hotmail.com>
2023-09-08 20:53:37 -0600

    cambio en about me

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -76,46 +76,26 @@ function About() {
         <div className=''>
           <h2 className="text-4xl font-bold mb-6 xl:mt-32">ABOUT ME</h2>
           <div className="max-w-2xl text-lg leading-relaxed mb-12">
-          <p>My name is Diana Lucía, and I'm on a journey to blend my love for concept development with the Art Direction.</p> <br />
+          <p>Hello there!</p> <br />
 
           <p>
-            I find inspiration in the everyday, and I believe that life is a canvas waiting to be
-            adorned with unforgettable moments.I thrive on crafting spaces and experiences
-            that resonate with people on a deeply emotional level.
+            My name is Diana Lucía, and I'm on a journey to blend my love for concept development with the art of direction.
           </p> <br />
 
           <p>
-            My fascination with creativity isn't just about aesthetics; it's about creating
-            environments that tell stories and evoke feelings.
-            I’m trying to develop everything since the narrative, a chapter in a larger story where
-            every detail matters. From the concept to the campaign or execution, it's all part of
-            the artistry that transforms an idea into something truly special.
+          I believe that life is a canvas waiting to be adorned with unforgettable moments. I thrive on crafting spaces and experiences that resonate with people on a deeply emotional level.
           </p> <br />
 
           <p>
-          As I embark on my journey in art direction, I'm excited to bring my passion for market
-research and design to a broader canvas. I see art direction as the perfect
-intersection of my love for aesthetics and my desire to create meaningful, immersive
-experiences. It's about shaping visual narratives that captivate, inspire, and leave a
-lasting impression.
+          My fascination with creativity isn't just about aesthetics; it's about creating environments that tell stories and evoke feelings. I’m trying to develop everything since the narrative, a chapter in a larger story where every detail matters. From the concept to the campaign or execution, it's all part of the artistry that transforms an idea into something truly special.
           </p><br />
 
           <p>
-          I see my job as a lifestyle, you'll find me exploring art galleries, doing some market
-research while sipping on a cup of artisanal herbal infusion, or wandering through
-nature to soak up inspiration from the world around me.
+          I see my job as a lifestyle, you'll find me exploring art galleries, doing some market research while sipping on a cup of artisanal herbal infusion, or wandering through nature to soak up inspiration from the world around me.
           </p><br />
 
-          <p>
-          I believe that every moment, whether big or small, is an opportunity to curate a
-beautiful experience.
+          <p>Join me on this exciting journey as I continue to explore the boundless possibilities of advertising and art direction. Let's create, inspire, and make every moment a masterpiece.
           </p><br />
-
-          <p>
-          Join me on this exciting journey as I continue to explore the boundless possibilities
-of advertising and art direction. Let's create, inspire, and make every moment a
-masterpiece.
-          </p>
           
           </div>
           <div className=''>
